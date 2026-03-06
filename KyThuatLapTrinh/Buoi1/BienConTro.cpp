93a7c043a5b6c0543539973b691fc0b85fa7ffea
PMT201 <PMT201@PMT3022>
2026-03-06 10:54:47 +0700

    test4

--- KyThuatLapTrinh/Buoi1/BienConTro.cpp
+++ KyThuatLapTrinh/Buoi1/BienConTro.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <vector> 
 using namespace std;
+struct SinhVien {
+	string hoten;
+	int tuoi;
+	bool gioitinh;
+	string diachi;
+};
 
 int main() {
-	int a = 5;
-	int* p = &a;
-	a += 6;
-	a = *p + 2;
-	cout << *p;
+	SinhVien b;
+	b.hoten = "Nguyen Van A";
+	b.tuoi = 20;
+	b.gioitinh = true;
+	b.diachi = "Tay Ninh";
+
+	SinhVien c;
+	c.hoten = "Tran Thi B";
+	c.tuoi = 30;
+
+	vector <SinhVien> mang;
+	mang.push_back(b);
+	mang.push_back(c);
+	for (int i = 0; i < mang.size(); i++) {
+		cout << "Sinh vien " << (i + 1) << endl;
+		cout << "\t+ Ho ten: " << mang[i].hoten << endl;
+		cout << "\t+ Tuoi: " << mang[i].tuoi << endl;
+	}
 }
