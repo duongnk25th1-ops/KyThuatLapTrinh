665983d9ace48f4014fe91dfe8795bd91ee79e32
PMT201 <PMT201@PMT3022>
2026-03-06 10:35:46 +0700

    test 3

--- KyThuatLapTrinh/Buoi1/BienConTro.cpp
+++ KyThuatLapTrinh/Buoi1/BienConTro.cpp
@@ -3,12 +3,9 @@
 using namespace std;
 
 int main() {
-	vector <float> diemthi;
-	diemthi.push_back(5.5);
-	diemthi.push_back(4);
-	diemthi.push_back(6);
-	diemthi.pop_back();
-	for (int i = 0; i < diemthi.size(); i++) {
-		cout << diemthi[i] << endl;
-	}
+	int a = 5;
+	int* p = &a;
+	a += 6;
+	a = *p + 2;
+	cout << *p;
 }
